be1f14814a89e4a1b284f19fb87b13147ce7d998
Marko Bauhardt <mb@datameer.com>
2017-01-27 13:08:32 +0100

    Improve printing

--- src/main/scala/datameer/configuration/Main.scala
+++ src/main/scala/datameer/configuration/Main.scala
@@ -8,8 +8,7 @@ import scala.io.StdIn
 
 object Main {
 
-
-  val HEADER_FORMAT = "%-50s %-80s %-80s %n";
+  val HEADER_FORMAT = "%-50s %-80s %-80s %n"
 
   def main(args: Array[String]): Unit = {
     print("Path to the new Datameer Installation: ")
@@ -22,44 +21,47 @@ object Main {
       .yieldPropertyFilesWithSameName(new File(oldDatameer + "/conf"), new File(newDatameer + "/conf"))
       .map(ConfigFiles.toPropertyFile)
 
+    val printf = (t: (String, String, String)) => print(String.format(HEADER_FORMAT, t._1, t._2, t._3))
+    val printf2 = (t: (String, String, String)) => print(String.format(HEADER_FORMAT, t._1, t._3, t._2))
 
-    printUpdatedEntries(propertyFiles)
-    printMissingEntries(propertyFiles)
-    printMissingEntries(propertyFiles.map((tuple: (PropertyFile, PropertyFile)) => (tuple._2, tuple._1)))
-
+    printUpdatedEntries(propertyFiles, printf)
+    printMissingEntries(propertyFiles, printf)
+    printMissingEntries(propertyFiles.map((tuple: (PropertyFile, PropertyFile)) => tuple.swap), printf2)
   }
 
-  private def printUpdatedEntries(propertyFiles: Stream[(PropertyFile, PropertyFile)]) = {
+
+  private def printUpdatedEntries(propertyFiles: Stream[(PropertyFile, PropertyFile)], printf: ((String, String, String)) => Unit) = {
     propertyFiles
       .foreach((tuple: (PropertyFile, PropertyFile)) => {
         val tuples = for {
           key1 <- tuple._1.properties.keys
           value1 <- tuple._1.properties.getValue(key1)
           value2 <- tuple._2.properties.getValue(key1)
-          if (value1 != value2)
+          if value1 != value2
         } yield (key1, value1, value2)
-        if (!tuples.isEmpty) {
+        if (tuples.nonEmpty) {
           println()
-          print(String.format(HEADER_FORMAT, "key", tuple._1.file, tuple._2.file))
-          tuples.foreach((t: (String, String, String)) => print(String.format(HEADER_FORMAT, t._1, t._2, t._3)))
+          println()
+          printf(("PropertyKey", tuple._1.file.getPath, tuple._2.file.getPath))
+          tuples.foreach(printf)
         }
       })
   }
 
-  private def printMissingEntries(propertyFiles: Stream[(PropertyFile, PropertyFile)]) = {
+  private def printMissingEntries(propertyFiles: Stream[(PropertyFile, PropertyFile)], printf: ((String, String, String)) => Unit) = {
     propertyFiles
       .foreach((tuple: (PropertyFile, PropertyFile)) => {
         val tuples = for {
           key1 <- tuple._1.properties.keys
           value1 <- tuple._1.properties.getValue(key1)
-          if (tuple._2.properties.getValue(key1).isEmpty)
+          if tuple._2.properties.getValue(key1).isEmpty
         } yield (key1, value1, "x")
-        if (!tuples.isEmpty) {
+        if (tuples.nonEmpty) {
           println()
-          print(String.format(HEADER_FORMAT, "key", tuple._1.file, tuple._2.file))
-          tuples.foreach((t: (String, String, String)) => print(String.format(HEADER_FORMAT, t._1, t._2, t._3)))
+          tuples.foreach((tuple: (String, String, String)) => tuple.copy(_1 = tuple._2))
+          printf(("PropertyKey", tuple._1.file.getPath, tuple._2.file.getPath))
+          tuples.foreach(printf)
         }
       })
   }
-
 }
